38fc7b3996080938813054332f2f44b99a882d2a
bolilyivs <bolilyivs@gmail.com>
2019-04-03 21:05:57 +0500

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,7 +1,7 @@
 from Server import Server
 
 if __name__ == '__main__':
-    server = Server("c71283221733a6f323993735ad971575ff4fc92ded92812cb2fbf3159e79e5768d1fdef874348ac3a83fc")
+    server = Server()
     server.run()
 
 
