93bf326bf647ace7c7475769c739383331868aca
lewan42 <42962709+lewan42@users.noreply.github.com>
2019-05-20 22:35:39 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,4 +3,8 @@ GROUP_ID = 180454656
 MODULES = ["GiveMeMoneyFinal", "HelloMsg",
            "ByeMsg", "WhoIMsg", "WhoYouMsg", "MondayFirstSchedule",
            "MondaySecondSchedule", "TuesdayFirstSchedule",
-           "TuesdaySecondSchedule"]
+           "TuesdaySecondSchedule", "GetAllDocuments"]
+
+DOCUMENTS = ["GetFile1", "GetFile2", "GetFile3", "GetFile4", "GetFile5", "GetFile6", "GetFile7", "GetFile8", "GetFile9",
+             "GetFile10", "GetFile11", "GetFile12", "GetFile13", "GetFile14", "GetFile15", "GetFile16", "GetFile17",
+             "GetFile18", "GetFile19", "GetFile20", "GetFile21", "GetFile22", "GetFile23"]
