c378c13b982962a5e02e035cde307593bcf7187f
Vladislav <bolilyivs@gmail.com>
2019-05-20 22:32:48 +0500

    Update docgen_messeges.py

--- messeges/docgen_messeges.py
+++ messeges/docgen_messeges.py
@@ -1,24 +1,62 @@
 from modules.message import Message
 from modules.xlsxparser import XLSXExporter
 
-class GetMoneyCourse(Message):
+class Statement(Message):
+    def getData(self):
+        return self.server.loadUserData(self.event)["data"]
+    def saveData(self, state):
+        self.server.saveUserData(self.event, state, "{}:{}".format(self.getData(), self.event.obj.text))
+    def clearData(self, state):
+        self.server.saveUserData(self.event, state, "")
+
+class GetMoney(Statement):
+    def request(self):
+        return r"(заполни заявление)|(заполни)"
+    def response(self):
+        self.clearData("gen_course")
+        return  "Введите ваш курс!"
+
+class GetMoneyCourse(Statement):
     def request(self):
         return r"\W+"
     def response(self):
-        self.server.saveUserData(self.event, 2, "data")
+        self.saveData("gen_group")
+        return  "Введите название группы!"
 
-
-class GiveMeMoneyFinal(Message):
+class GetMoneyGroup(Statement):
     def request(self):
-        return "заявление на стипендию"
+        return r"\W+"
     def response(self):
-        values = {
-            "course" : "",
-            "group" : "",
-            "fio" : self.user.last_name + " " + self.user.first_name[0],
-            "mobile" : "",
-            "active" : ""}
-        self.server.upload_gen_document(self.event, "documents/SchApp.docx", values, "Степуха!")
+        self.saveData("gen_secondName")
+        return  "Введите свое отчество!"
 
+class GetMoneySecondName(Statement):
+    def request(self):
+        return r"\W+"
+    def response(self):
+        self.saveData("gen_mobile")
+        return  "Введите номер мобильного телефона!"
+
+class GetMoneyMobile(Statement):
+    def request(self):
+        return r"\W+"
+    def response(self):
+        self.saveData("gen_active")
+        return  "Введите: за какую активность стипендия? "
+
+class GiveMeMoneyFinal(Statement):
+    def request(self):
+        return r"\W+"
+    def response(self):
+        data = self.getData().split(":")
+
+        values = {
+            "course" : data[0],
+            "group" : data[1],
+            "fio" : self.user.last_name + " " + self.user.first_name[0] + " " + data[2],
+            "mobile" : data[3],
+            "active" : data[4]}
+        self.server.upload_gen_document(self.event, "documents/SchApp.docx", values, "Степуха!")
+        self.clearData("default")
         return  "Готово!"
 
